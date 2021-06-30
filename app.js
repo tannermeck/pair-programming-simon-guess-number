f724798aae22061dce61670bd0d6c50c4f71f9a1
Simon Kaine <simonkaine@gmail.com>
2021-06-30 12:38:45 -0700

    Partial Wireframe + grab DOM elements

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // import functions and grab DOM elements
 
 // initialize state
+const btn = document.getElementById('btn-id');
+const resultsSpan = document.getElementById('span-id');
+const userGuess = document.getElementById('user-guess-id');
+
+const targetNumber = Math.floor(Math.random()*20);
+const guessesRemaining = 4; 
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
