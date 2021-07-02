ba0ce52844a1a6a59c1444f7e0deb2ea822b8ea4
TANNER MECK <steve.meck246@gmail.com>
2021-07-02 13:41:12 -0700

    tanners changes comments removed console log removed correct spacing math ceil added reset button working

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-// import functions and grab DOM elements
 import { compareNumbers } from './utils.js';
 
-// initialize state
 const btn = document.getElementById('btn-id');
 const resetBtn = document.getElementById('reset-id');
 const resultsSpan = document.getElementById('span-id');
@@ -9,22 +7,14 @@ const triesLeftSpan = document.getElementById('triesSpan-id');
 const highOrLowSpan = document.getElementById('highOrLowSpan-id');
 const userGuess = document.getElementById('user-guess-id');
 let guessesRemaining = 4; 
-// console.log(userGuess);
-const targetNumber = Math.floor(Math.random() * 20);
 
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const targetNumber = Math.ceil(Math.random() * 20);
 
 btn.addEventListener('click', ()=> {
 
     guessesRemaining--;
     const guessValue = userGuess.value;
     let numberGuess = Number(guessValue);
-
-    console.log(targetNumber, numberGuess);
 
     if (numberGuess < 0 || numberGuess > 20) {
         alert('Please choose a number between 1 and 20.');
@@ -53,37 +43,11 @@ btn.addEventListener('click', ()=> {
         triesLeftSpan.textContent = `You have ${guessesRemaining} guesses left`;
     } else {
         return;
-    }
+    } 
 });
 
 resetBtn.addEventListener('click', ()=> {
-    guessesRemaining = 4;
-    btn.disabled = false;
-    userGuess.value = '';
+    location.reload();
 });
 
 
-  // ---------------------IF/ELSE CODE PRIOR TO REFACTORING, below..
-
-  // if (numberGuess === targetNumber || guessesRemaining === 0) {
-  //   resultsSpan.textContent = 'You guessed right!! Congrats!';
-  //   highOrLowSpan.style.display = 'none';
-  //   triesLeftSpan.style.display = 'none';
-  //   btn.disabled = true;
-  //   if (guessesRemaining === 0) {
-  //     resultsSpan.textContent = 'No more guesses! Game over!';
-  //     triesLeftSpan.style.display = 'none';
-  //   }
-  // }
-  // else if (numberGuess < targetNumber) {
-  //   highOrLowSpan.textContent = 'Your guess is too LOW.';
-  //   resultsSpan.textContent = "Keep guessing..";
-  //   triesLeftSpan.textContent = `You have ${guessesRemaining} guesses left`;
-  // } 
-  // else if (numberGuess > targetNumber) {
-  //   highOrLowSpan.textContent = 'Your guess is too HIGH.';
-  //   resultsSpan.textContent = "Keep guessing..";
-  //   triesLeftSpan.textContent = `You have ${guessesRemaining} guesses left`;
-  // } else {
-  //   return;
-  // }
